d0593325967e8e387c7bb5336624d3ed80bbc804
jasmin <jasminchakma895@gmail.com>
2023-11-12 21:07:46 +0600

    remote route

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter} from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home/Home";
-// import About from "../Components/About/About";
-// import ContactUs from "../Components/ContactUs/ContactUs";
-// import Service from "../Components/Service/Service";
 
 const Router = createBrowserRouter([
   {
@@ -14,18 +11,6 @@ const Router = createBrowserRouter([
         index: true,
         element: <Home></Home>,
       },
-      // {
-      //   path : 'about',
-      //   element: <About></About>
-      // },
-      // {
-      //   path : 'contact-us',
-      //   element : <ContactUs> </ContactUs>
-      // },
-      // {
-      //   path : 'service',
-      //   element : <Service></Service>
-      // }
     ],
   },
 ]);
